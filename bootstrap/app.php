8eabf268a64fe3523cc22f95111117c5c7949081
Labib Abdullah <labibabdullahhsan@gmail.com>
2025-12-04 17:17:11 +0700

    add role middleware for user access control

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\Routing\Alias;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => App\Http\Middleware\role::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
